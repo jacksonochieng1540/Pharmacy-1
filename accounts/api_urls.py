bc27f02c96c59b2149d0d23bd8fa45f5cb559ac2
ayagah <jacksonochieng1540@gmail.com>
2025-12-05 16:12:05 +0300

    added the api_urls.py

--- accounts/api_urls.py
+++ accounts/api_urls.py
@@ -4,9 +4,11 @@ from . import api_views
 
 router = DefaultRouter()
 router.register('users', api_views.UserViewSet, basename='user')
+router.register('activities', api_views.UserActivityViewSet, basename='activity')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('login/', api_views.LoginAPIView.as_view(), name='api_login'),
-    path('logout/', api_views.LogoutAPIView.as_view(), name='api_logout'),
+    path('login/', api_views.LoginAPIView.as_view(), name='login'),
+    path('logout/', api_views.LogoutAPIView.as_view(), name='logout'),
+    path('refresh-token/', api_views.refresh_token, name='refresh_token'),
 ]
